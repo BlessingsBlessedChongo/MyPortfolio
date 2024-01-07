2a1bc032fe33eb6fe70a5889f6befa0ac1b6f901
Blessings Blessed Chongo <155776973+BlessingsBlessedChongo@users.noreply.github.com>
2024-01-08 00:35:20 +0200

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/myimage.jpg";
+import image from "../images/BBC.jpg";
 
 const imageAltText = "Blessings standing on a podium giving a presentation.";
 
