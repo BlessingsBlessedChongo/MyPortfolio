a6c4b434f222ffb47822296e9ded84fd1fc9567a
Blessings Blessed Chongo <155776973+BlessingsBlessedChongo@users.noreply.github.com>
2024-01-07 15:32:46 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Blessings Blessed Chongo",
+  title: "Computer Science student, Microsoft Learns Student Ambassador",
+  email: "blessingschongo796@gmail.com",
+  gitHub: "htpps://github.com/BlessingsBlessedChongo",
+  instagram: " ",
+  linkedIn: "https://linkedin.com/in/blessings-chongo-9661a5266",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: " ",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
